2bc73c3d51aee8611f623272228947c3395e227a
Scott, Jason <jscott@luxoft.com>
2023-11-21 14:28:39 +0100

    Init

---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.96.0"
+    }
+  }
+}
+
+resource "azurerm_resource_group" "tf_test" {
+    name = "tfmainresourcegroup"
+    location = "centralus"
+}
+
+resource "azurerm_container_group" "tfcg_group" {
+  name = "weatherapi"
+  location = azurerm_resource_group.tf_test.location
+  resource_group_name = azurerm_resource_group.tf_test.name
+  
+  ip_address_type = "Public"
+  dns_name_label = "popuppopwa"
+  os_type = "Linux"
+
+  container {
+      name = "weatherapi"
+      image = "popuppop/weatherapi"
+      cpu = "1"
+      memory = "1"
+      environment_variables = {
+        "WEBSITES_CONTAINER_START_TIME_LIMIT" = 1800
+      }
+
+      ports {
+        port = 80
+        protocol = "TCP"
+      } 
+  }
+}
