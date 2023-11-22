48b386b40f42026f834ed4711ec751e4e145f159
Scott, Jason <jscott@luxoft.com>
2023-11-22 08:12:45 +0100

    First provisioning

--- main.tf
+++ main.tf
@@ -3,6 +3,12 @@ provider "azurerm" {
 }
 
 terraform {
+  backend "azurerm" {
+    resource_group_name = "tf_rg_blobs_store"
+    storage_account_name = "tfstorageaccountpopuppop"
+    container_name = "tfstate"
+    key = "terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
